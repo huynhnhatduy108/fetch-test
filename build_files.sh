112e964debcf0b62c879f9b7d24bf4511a1f6cb9
Huynh Nhat Duy <huynhnhatduy2112@gmail.com>
2023-02-19 11:26:01 +0700

    update code

--- build_files.sh
+++ build_files.sh
@@ -3,8 +3,8 @@ echo "Building files...."
 pip install -r requirements.txt
 
 echo "Migration...."
-python manage.py makemigrations models  
-python manage.py migrate models
+python3.9 manage.py makemigrations models  
+python3.9 manage.py migrate models
 
 echo "Collectstatic...."
 python3.9 manage.py collectstatic
