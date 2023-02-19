cc4c5a0e885f2c724960d93bf6f140af51899bd7
Huynh Nhat Duy <huynhnhatduy2112@gmail.com>
2023-02-19 12:42:41 +0700

    updtae seting

--- src/settings.py
+++ src/settings.py
@@ -147,9 +147,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
-STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
@@ -157,4 +158,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = BASE_DIR /'staticfiles_build'/'static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
