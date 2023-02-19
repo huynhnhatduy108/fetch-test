513db1e8ff30efbe020aa8f7ef2cbe722ff6fdee
Huynh Nhat Duy <huynhnhatduy2112@gmail.com>
2023-02-19 11:29:12 +0700

    updtae

--- build_files.sh
+++ build_files.sh
@@ -2,7 +2,7 @@
 echo "Building files...."
 pip install -r requirements.txt
 
-echo "Migration...."
+echo "Make migration...."
 python3.9 manage.py makemigrations models  
 python3.9 manage.py migrate models
 
